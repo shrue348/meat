5249fcaf57e8cd45795d65c9e6b208671cc517cf
shrue348 <shure348@gmail.com>
2017-04-03 17:09:32 +0300

    commit

--- js/ready.js
+++ js/ready.js
@@ -6,3 +6,23 @@ $(()=>{
 	$('.card_gal').slick();
 });
 
+
+$(()=>{ //mob_menu
+	$('.mob_menu__switch').on('click', ()=>{
+		$('.mob_menu__wrap').addClass('active');
+		$('.mob_menu__overlay').addClass('active');
+		$('.main_wrap').addClass('active');
+	});
+
+	$('.mob_menu__overlay').on('click', ()=>{
+		$('.mob_menu__wrap').removeClass('active');
+		$('.mob_menu__overlay').removeClass('active');
+		$('.main_wrap').removeClass('active');
+	});
+
+	$(window).scroll(()=>{
+		$('.mob_menu__wrap').removeClass('active');
+		$('.mob_menu__overlay').removeClass('active');
+		$('.main_wrap').removeClass('active');
+	})
+});
